b850d556c5c29027a40b6bd2d51265d20f29e3cf
Josue Laurentino <josue.laurentino@csptecnologia.com>
2022-05-20 18:26:32 -0300

    páginas estáticas e navegação finalizados

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyB4Loz_4OITG-Sb7x9PhuuQcGAKbBTwn_A",
+    authDomain: "projeto-integrador-fc1da.firebaseapp.com",
+    projectId: "projeto-integrador-fc1da",
+    storageBucket: "projeto-integrador-fc1da.appspot.com",
+    messagingSenderId: "971085859001",
+    appId: "1:971085859001:web:027fad0e8b05220bb5b368"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
